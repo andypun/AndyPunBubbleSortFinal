41ed73b47a5a2a37c9beb753039a722de7c400b9
BT_1N3_26 <apun6146@bths.edu>
2019-01-03 08:39:16 -0500

    1/3/19

--- src/AndyPun/BubbleSort.java
+++ src/AndyPun/BubbleSort.java
@@ -3,6 +3,12 @@ package AndyPun;
 public class BubbleSort {
     public static void swap(int[] Arr, int i,int j)
     {
-        
+       int temp = Arr[i];
+       Arr[i] = Arr[j];
+       Arr[j] = temp;
     }
+
+
+
+
 }
